43bef5335df617fd597c4c4f5d3559fae9f3e84e
Tickmon <santiago.1701423560@ucaldas.edu.co>
2020-04-20 23:43:30 -0500

    nueva barra de busqueda

--- index.php
+++ index.php
@@ -8,17 +8,14 @@
         <link rel="stylesheet" href="styles/css/style_1.css">
     </head>
     <body>
-    <div class="container">
-        <header class="jumbotron mainheader">
-            <img src="Images/logo_final.png" alt="logo">
+    <div class="container mainheader">
+        <header class="text-center">
+            <a href="#"><img src="Images/logo_final.png" alt="logo"></a>
         </header>
     </div>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg justify-content-center mainnav">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Inicio</a>
-                </li>
+    <div class="container mainnav">
+        <nav class="navbar navbar-expand-lg justify-content-center">
+            <ul class="nav list">
                 <li class="nav-item">
                     <a class="nav-link nvi" href="#">Desechables</a>                 
                 </li>
@@ -34,8 +31,19 @@
                 <li class="nav-item">
                     <a class="nav-link nvi" href="#">Otros</a>
                 </li>
+                <li class="divider"></li>
             </ul>
         </nav>
+        <form class="searchnav" action="" method="">
+            <div class="form-row">
+                <div class="col-sm-8">
+                    <input type="search" class="form-control" placeholder="Hola! ¿Que estás buscando?">
+                </div>     
+                <div class="col-sm-3">
+                    <button type="submit" class="btn btn-block searchbtn">Buscar</button>
+                </div> 
+            </div>
+        </form>  
     </div>
         <script href="styles/jsb/jquery-3.5.0.min.js"></script>
         <script href="styles/jsb/popper.min.js"></script>

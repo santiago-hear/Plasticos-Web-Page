0925dd684103a19f34b436fe9f2a769d44183ac1
Tickmon2 <santiago.hernandez124@gmail.com>
2020-04-22 01:47:13 -0500

    nuevo banner, cambio de cabecero y colores

--- index.php
+++ index.php
@@ -3,50 +3,120 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Plásticos LA 17</title>
+        <title>Plásticos LA 17, !Todo en plásticos y desechables!</title>
         <link rel="stylesheet" href="styles/cssb/bootstrap.min.css">
         <link rel="stylesheet" href="styles/css/style_1.css">
     </head>
     <body>
-    <div class="container mainheader">
-        <header class="text-center">
-            <a href="#"><img src="Images/logo_final.png" alt="logo"></a>
-        </header>
-    </div>
-    <div class="container mainnav">
-        <nav class="navbar navbar-expand-lg justify-content-center">
-            <ul class="nav list">
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Desechables</a>                 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Bolsas</a>               
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Plásticos</a>  
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Alimentos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nvi" href="#">Otros</a>
-                </li>
-                <li class="divider"></li>
-            </ul>
-        </nav>
-        <form class="searchnav" action="" method="">
-            <div class="form-row">
-                <div class="col-sm-8">
-                    <input type="search" class="form-control" placeholder="Hola! ¿Que estás buscando?">
-                </div>     
-                <div class="col-sm-3">
-                    <button type="submit" class="btn btn-block searchbtn">Buscar</button>
-                </div> 
+        <div class="mainheader">  
+            <nav class="navbar navbar-expand-lgr">
+                <a href="#" class="navbar-brand"><img src="Images/logo_3.png" alt="logo"></a>
+                <ul class="nav list">
+                    <li class="nav-item">
+                        <a class="nav-link nvi" href="#">Conócenos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nvi" href="#">Contacto</a>          
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nvi" href="#">Productos</a>  
+                    </li>
+                    <!--
+                    <li class="nav-item">
+                        <a class="nav-link nvi" href="#">Alimentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nvi" href="#">Más</a>
+                    </li>
+                    -->
+                </ul>
+                <form action="" class="navform">
+                    <button class="form-control sign_in_btn">Inicia Sesión</button>
+                </form>
+            </nav>
+            <form class="searchnav" action="" method="get">
+                <div class="form-row">
+                    <div class="col-sm-8">
+                        <input type="search" class="form-control" placeholder="Hola! ¿Qué estás buscando?">
+                    </div>     
+                    <div class="col-sm-3">
+                        <button type="search" class="form-control searchbtn">Buscar</button>
+                    </div> 
+                </div>
+            </form>  
+        </div>
+        <div class="banner">
+            <div id="demo" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                    <li data-target="#demo" data-slide-to="3"></li>
+                    <li data-target="#demo" data-slide-to="4"></li>
+                </ul>
+
+                <!-- The slideshow -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="Images/aluminio.jpg" alt="Papel Aluminio">
+                        <div class="carousel-caption msg">
+                            <a href="">
+                                <h2>Envoltorios</h2>
+                                <p>Peliculas transparentes y aluminios...</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/platos_alimentos.jpg" alt="Portacomidas">
+                        <div class="carousel-caption msg">
+                            <a href="">
+                                <h2>Desechables</h2>
+                                <p>Platos, vasos y cubiertos...</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/fsecos.jpg" alt="Frutos Secos">
+                        <div class="carousel-caption msg">
+                            <a href="">
+                                <h2>Frutos Secos</h2>
+                                <p>Coco, Almendras, Maní y más...</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/palillos.jpeg" alt="Palillos">
+                        <div class="carousel-caption msg">
+                            <a href="">
+                                <h2>Madera</h2>
+                                <p>Palos, Palillos y Pinchos...</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/bolsas4.jpg" alt="Bolsas">
+                        <div class="carousel-caption msg">
+                            <a href="">
+                                <h2>Bolsas</h2>
+                                <p>Bolsas de papel, plásticas y más...</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
             </div>
-        </form>  
-    </div>
-        <script href="styles/jsb/jquery-3.5.0.min.js"></script>
-        <script href="styles/jsb/popper.min.js"></script>
-        <script href="styles/jsb/bootstrap.min.js"></script>
+        </div>
+        <script src="styles/jsb/jquery-3.5.0.min.js"></script>
+        <script src="styles/jsb/popper.min.js"></script>
+        <script src="styles/jsb/bootstrap.min.js"></script>
     </body>
 </html>
